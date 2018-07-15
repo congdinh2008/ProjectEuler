ba3ad69a0df8f0a0869e3a8b747ed84a870c9be6
Cong Dinh <congdinh2012@hotmail.com>
2018-07-15 17:15:53 +0700

    Add Problem 14-17

---
+++ ProjectEuler/Problem014/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Problem014
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {            
+            long sequenceLength = 0;
+            long sequence;
+            long startNum = 0;
+
+            Stopwatch clock = Stopwatch.StartNew();
+
+            int[] cache = new int[1000001];
+
+            for (int i = 0; i < cache.Length; i++)
+            {
+                cache[i] = -1;
+            }
+            cache[1] = 1;
+
+            for(int i=2; i <= 1000000;i++)
+            {
+                int k = 0;
+                sequence = i;
+                while (sequence != 1&&sequence>=i)
+                {
+                    k++;
+                    if (sequence % 2 == 0)
+                    {
+                        sequence = sequence / 2;
+                    }
+                    else
+                    {
+                        sequence = 3 * sequence + 1;
+                    }
+                }
+
+                cache[i] = k + cache[sequence];
+
+                if (cache[i] > sequenceLength)
+                {
+                    sequenceLength = cache[i];
+                    startNum = i;
+                }
+            }
+
+            clock.Stop();
+            
+            Console.WriteLine("The starting number {0} produces a sequence of {1}",startNum, sequenceLength);
+            Console.WriteLine("Solution took {0}ms", clock.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectEuler/Problem016/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Problem016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger num = 1;
+            BigInteger sum = 0;
+
+            Stopwatch clock = Stopwatch.StartNew();
+            for (int i = 0; i < 1000; i++)
+            {
+                num *= 2;
+            }
+
+            do
+            {
+                sum += num % 10;
+                num = num / 10;
+            } while (num >= 10);
+
+            sum += num;
+
+            clock.Stop();
+
+            Console.WriteLine(sum);
+            Console.WriteLine("Solution took {0}ms", clock.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
